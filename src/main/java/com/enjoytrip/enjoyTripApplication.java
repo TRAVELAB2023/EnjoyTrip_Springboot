030ac49c6778add27eaa7e26db5e3744a225d858
genboyjy <pingo_ov@naver.com>
2023-05-02 09:14:56 +0900

    Initial commit

---
+++ src/main/java/com/enjoytrip/enjoyTripApplication.java
@@ -0,0 +1,13 @@
+package com.enjoytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class enjoyTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(enjoyTripApplication.class, args);
+	}
+
+}
