7fdd1086ae70845fa2a15202402b44f4b23783b9
genboyjy <genboyjy@gmail.com>
2023-05-16 14:28:34 +0900

    feat(board): board & commentsBoard model

--- src/main/java/com/enjoytrip/model/Board.java
+++ src/main/java/com/enjoytrip/model/Board.java
@@ -1,4 +1,32 @@
 package com.enjoytrip.model;
 
-public class Board {
+import com.enjoytrip.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "board")
+public class Board extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    int boardId;
+    String title;
+    int hit;
+    String content;
+    @Column(name="del_yn")
+    boolean delYn;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", updatable = false)
+    private List<CommentBoard> commentBoardList;
 }
--- src/main/java/com/enjoytrip/model/CommentBoard.java
+++ src/main/java/com/enjoytrip/model/CommentBoard.java
@@ -1,4 +1,33 @@
 package com.enjoytrip.model;
 
-public class CommentBoard {
+import com.enjoytrip.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "comments_board")
+public class CommentBoard extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="comments_id")
+    int commentId;
+
+    String content;
+    @Column(name = "board_id")
+    int boardId;
+    @Column(name = "member_id")
+    int memberId;
+
+    int rgruop;
+    @Column(name = "reply_depth")
+    boolean replyDepth;
+
 }
