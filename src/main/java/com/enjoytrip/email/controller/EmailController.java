f7846095762fcaf0b037ccc4b5beb707eeaaa319
김세혁 <se6816@naver.com>
2023-05-25 09:55:00 +0900

    fix(email) : add Email 변경시 임시비밀번호 발급

--- src/main/java/com/enjoytrip/email/controller/EmailController.java
+++ src/main/java/com/enjoytrip/email/controller/EmailController.java
@@ -1,5 +1,6 @@
 package com.enjoytrip.email.controller;
 
+import com.enjoytrip.email.dto.EmailRequestDto;
 import com.enjoytrip.email.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,20 +24,15 @@ public class EmailController {
     }
 
 
-    @GetMapping("/recovery/{email}")
-    public ResponseEntity<Void> sendEmail(HttpSession session,@PathVariable @Email(regexp = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",
-            message = "이메일 양식이 맞지 않습니다.") String email){
-        emailService.sendEmail(email,session);
+    @PostMapping("/recovery")
+    public ResponseEntity<Void> sendEmail(@RequestBody @Valid EmailRequestDto emailRequestDto){
+        emailService.sendEmail(emailRequestDto);
         return new ResponseEntity(HttpStatus.OK);
     }
     @GetMapping("/check/{auth}")
-    public ResponseEntity<Void> authCheck(HttpSession session, @PathVariable String auth){
-        String storeAuth=(String)session.getAttribute("auth");
-        if(storeAuth==auth){
-            return new ResponseEntity(HttpStatus.OK);
-        }else{
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Void> authCheck( @PathVariable String auth){
+        emailService.checkEmail(auth);
+        return new ResponseEntity(HttpStatus.OK);
 
     }
 
--- src/main/java/com/enjoytrip/email/service/EmailService.java
+++ src/main/java/com/enjoytrip/email/service/EmailService.java
@@ -1,8 +1,12 @@
 package com.enjoytrip.email.service;
 
 
+import com.enjoytrip.email.dto.EmailRequestDto;
+
 import javax.servlet.http.HttpSession;
 
 public interface EmailService {
-    void sendEmail(String email, HttpSession session);
+    void sendEmail(EmailRequestDto emailRequestDto);
+
+    void checkEmail(String key);
 }
